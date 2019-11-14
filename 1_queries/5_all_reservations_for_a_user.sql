2c3eebb736c2999f114797729f9f18321132f5fc
hanuz06 <lee1975uz@gmail.com>
2019-11-14 17:33:47 +0000

    add sql query to getAllReservations function

--- 1_queries/5_all_reservations_for_a_user.sql
+++ 1_queries/5_all_reservations_for_a_user.sql
@@ -1,23 +1,21 @@
 SELECT
-  properties.id,
-  properties.title,
-  properties.cost_per_night,
-  reservations.start_date,
+  properties.*,
+  reservations.*,
   AVG(rating) as average_rating
 FROM
-reservations  
+  reservations
   JOIN properties ON reservations.property_id = properties.id
-  JOIN property_reviews ON property_reviews.property_id = properties.id 
+  JOIN property_reviews ON property_reviews.property_id = properties.id
 WHERE
-  reservations.guest_id = 1 AND reservations.end_date <= now()::date
+  reservations.guest_id = 1
+  AND reservations.end_date <= now() :: date
 GROUP BY
   reservations.id,
   properties.id
 ORDER BY
-  reservations.start_date 
+  reservations.start_date
 LIMIT
   10;
-
 
 -- --Correct answer
 -- SELECT properties.*, reservations.*, avg(rating) as average_rating
@@ -29,4 +27,3 @@ LIMIT
 -- GROUP BY properties.id, reservations.id
 -- ORDER BY reservations.start_date
 -- LIMIT 10;
-
